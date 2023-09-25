a8d6d160e962a5df5b468efff149d30101747a67
SnehaGhosh4700 <140139277+SnehaGhosh4700@users.noreply.github.com>
2023-09-25 18:34:20 +0530

    All home page javascripts

---
+++ Co HomePage.js
@@ -0,0 +1,4 @@
+// Footer
+const year = new Date().getFullYear();
+    const yearElement = document.querySelector('.footer-content p');
+   // yearElement.textContent = `&copy; ${year}Sneha Ghosh.All Rights are reserved.CODE HUB,Sodepur. ` ;

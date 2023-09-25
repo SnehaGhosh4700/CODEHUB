6793b495875a156a94b3682929cd4043431eb675
SnehaGhosh4700 <140139277+SnehaGhosh4700@users.noreply.github.com>
2023-09-25 18:31:56 +0530

    Header Changes

---
+++ Co 3 Header.js
@@ -0,0 +1,10 @@
+function change_header()
+{
+    Header.innerHTML="<marquee behavior=alternate>Welcome to CODE HUB Sodepur</marquee>"
+    Header.style.color="green"
+}
+function default_header()
+{
+    Header.innerHtml="Let's Explore new Things!!"
+   Header.style.color="blue"
+}
